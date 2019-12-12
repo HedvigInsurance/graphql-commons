39dbf349c418944afea1c539e0c0945dc7b3bd2a
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2019-12-12 17:34:45 +0100

    working subscription

--- src/main/java/com/hedvig/graphql/commons/extensions/DataFetchingEnviromentExtensions.kt
+++ src/main/java/com/hedvig/graphql/commons/extensions/DataFetchingEnviromentExtensions.kt
@@ -1,17 +1,18 @@
 package com.hedvig.graphql.commons.extensions
 
 import graphql.schema.DataFetchingEnvironment
-import graphql.servlet.context.GraphQLServletContext
+import graphql.servlet.GraphQLContext
 
 private const val HEDVIG_TOKEN: String = "hedvig.token"
 private const val ACCEPT_LANGUAGE: String = "Accept-Language"
 
-fun DataFetchingEnvironment.getToken() =
-    this.getContext<GraphQLServletContext?>()?.httpServletRequest?.getHeader(HEDVIG_TOKEN)
-        ?: throw NullPointerException("$HEDVIG_TOKEN is missing!")
+fun DataFetchingEnvironment.getToken(): String =
+    this.getContext<GraphQLContext>()!!.httpServletRequest
+        .map { r -> r.getHeader(HEDVIG_TOKEN) }
+        .orElseThrow { throw NullPointerException("$HEDVIG_TOKEN is missing") }
 
-fun DataFetchingEnvironment.getTokenOrNull() =
-    this.getContext<GraphQLServletContext?>()?.httpServletRequest?.getHeader(HEDVIG_TOKEN)
+fun DataFetchingEnvironment.getTokenOrNull(): String? =
+    this.getContext<GraphQLContext>()?.httpServletRequest?.map { r -> r.getHeader(HEDVIG_TOKEN) }?.orElse(null)
 
-fun DataFetchingEnvironment.getAcceptLanguage() =
-    this.getContext<GraphQLServletContext?>()?.httpServletRequest?.getHeader(ACCEPT_LANGUAGE)
+fun DataFetchingEnvironment.getAcceptLanguage(): String? =
+    this.getContext<GraphQLContext>()?.httpServletRequest?.map { r -> r.getHeader(ACCEPT_LANGUAGE) }?.orElse(null)
