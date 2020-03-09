436a06299f858ae3901adb26a06496839bd31520
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-03-09 17:16:02 +0100

    added getEndUserIp

--- src/main/java/com/hedvig/graphql/commons/extensions/DataFetchingEnviromentExtensions.kt
+++ src/main/java/com/hedvig/graphql/commons/extensions/DataFetchingEnviromentExtensions.kt
@@ -5,6 +5,7 @@ import graphql.servlet.context.GraphQLServletContext
 
 private const val HEDVIG_TOKEN: String = "hedvig.token"
 private const val ACCEPT_LANGUAGE: String = "Accept-Language"
+private const val X_FORWARDED_FOR: String = "x-forwarded-for"
 
 fun DataFetchingEnvironment.getToken() =
     this.getContext<GraphQLServletContext?>()?.httpServletRequest?.getHeader(HEDVIG_TOKEN)
@@ -15,3 +16,12 @@ fun DataFetchingEnvironment.getTokenOrNull() =
 
 fun DataFetchingEnvironment.getAcceptLanguage() =
     this.getContext<GraphQLServletContext?>()?.httpServletRequest?.getHeader(ACCEPT_LANGUAGE)
+
+fun DataFetchingEnvironment.getEndUserIp(): String? {
+    val ip = this.getContext<GraphQLServletContext?>()?.httpServletRequest?.getHeader(X_FORWARDED_FOR)
+    return if (ip?.contains(",") == true) {
+        ip.split(",").first()
+    } else {
+        ip
+    }
+}
